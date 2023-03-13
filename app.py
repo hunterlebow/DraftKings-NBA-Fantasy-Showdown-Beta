d003f075c96fbf6b01e0cc4cb80f233b3d30ce32
Hunter Lebow <hunterlebow@gmail.com>
2023-03-13 04:35:38 -0400

    table pagination

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 import gurobipy as gb
 import pandas as pd
-
+import math
 
 app = Flask(__name__)
 CORS(app)
+
 
 BUDGET = 50000
 CAPTAIN_REQ = 1
 CAPTAIN_BOOST = 1.5
 UTIL_REQ = 5
 TEAM_REQ = 6
+# Figure this out....
+RED_ASTERIK = "*"
+RESULTS_PER_PAGE = 1
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -21,11 +25,17 @@ def index():
         if not file:
             return jsonify({"Error": "No File Uploaded"}), 400
 
-        obj_val, data = optimize(file)
+        db = optimize(file)
 
-        return jsonify(data)
+        page = request.args.get("page", default=1, type=int)
 
-        #work on getting obj val in the front end client side....
+        page_start = (page - 1) * RESULTS_PER_PAGE
+        page_end = page_start + RESULTS_PER_PAGE
+
+        # page_data = db[page_start:page_end]
+        return jsonify(db)
+
+        # work on getting obj val in the front end client side....
 
     else:
         return render_template("index.html")
@@ -42,6 +52,9 @@ def optimize(f):
     players = df.index.tolist()
 
     m = gb.Model()
+
+    m.setParam("PoolSearchMode", 2)
+    m.setParam("PoolSolutions", 10)
 
     captain = m.addVars(players, vtype=gb.GRB.BINARY, name="captain")
     team = m.addVars(players, vtype=gb.GRB.BINARY, name="team")
@@ -62,39 +75,52 @@ def optimize(f):
                 for p in players) == CAPTAIN_REQ, "Captain")
 
     m.addConstr(captain.sum() == CAPTAIN_REQ, "CaptainCount")
-    m.addConstr(team.sum() == UTIL_REQ, "Utility Count")
+    m.addConstr(team.sum() == UTIL_REQ, "UtilityCount")
+    m.addConstr(gb.quicksum(team[p] + captain[p] for p in players) == TEAM_REQ, "TeamSize")
     m.addConstrs((team[p] + captain[p] <= 1 for p in players), "TeamComp")
 
     # Optimize the model
     m.optimize()
 
-    data = []
+    # print("\n\nJSON SOLUTION: ", m.getJSONSolution(), "\n\n")
 
-    for p in players:
-        if m.getVarByName("captain[{}]".format(p)).X == 1:
-            data.append(
-                {
-                    "Status": "Captain",
-                    "Name": df.loc[p, "Player"],
-                    "Projected Points": float(df.loc[p, "FPPG"]),
-                    "Cost": float(df.loc[p, "Salary"])
-                }
-            )
+    db = {}
 
-        elif m.getVarByName("team[{}]".format(p)).X == 1:
-            data.append(
-                {
-                    "Status": "Utility",
-                    "Name": df.loc[p, "Player"],
-                    "Projected Points": float(df.loc[p, "FPPG"]),
-                    "Cost": float(df.loc[p, "Salary"])
-                }
-            )
+    for sol in range(m.SolCount):
 
-        else:
-            continue
+        m.setParam("SolutionNumber", sol)
 
-    return m.objVal, {"data": data}
+        page_data = []
+        for p in players:
+            if m.getVarByName("captain[{}]".format(p)).Xn == 1:
+                page_data.append(
+                    {
+                        "Status": "Captain",
+                        "Name": df.loc[p, "Player"],
+                        "Projected Points": float(df.loc[p, 'FPPG'])*CAPTAIN_BOOST,
+                        "Cost": f"{df.loc[p, 'Salary']*CAPTAIN_BOOST}{RED_ASTERIK}"
+                    }
+                )
+
+            elif m.getVarByName("team[{}]".format(p)).Xn == 1:
+                page_data.append(
+                    {
+                        "Status": "Utility",
+                        "Name": df.loc[p, "Player"],
+                        "Projected Points": float(df.loc[p, "FPPG"]),
+                        "Cost": float(df.loc[p, "Salary"])
+                    }
+                )
+
+            else:
+                continue
+        print("\n\nPAGE DATA", page_data, "\n\n")
+
+        db[m.PoolObjVal] = sorted(sorted(
+            page_data, key=lambda x: x["Status"]), key=lambda x: x["Projected Points"], reverse=True)
+        # m.setParam("PoolGap", 0.001)
+
+    return db
 
     # To work on, find multiple solutions (PoolSearch Parameter?) and store them...
     # ... in s in a table (or at least on the front end)
--- static/script.js
+++ static/script.js
@@ -1,45 +1,108 @@
 const form = document.getElementById("form-data");
 const fileInput = document.getElementById("file");
+const tableBody = document.getElementById("table-body");
+const nextPageButton = document.getElementById("next-btn");
+const prevPageButton = document.getElementById("prev-btn");
+const objValDiv = document.getElementById("obj-val");
+
+let currentPage = 0; // Index of the current page
+let arrayData = null; // Store the fetched data as an array
+let objectData = null; //Store the fetched raw object data (dictionary)
+
+function renderPage(pageIndex) {
+  tableBody.innerHTML = "";
+  console.log(arrayData);
+  let pageData = arrayData[pageIndex];
+  console.log(pageData);
+  let objVal = pageData[0];
+  let lineup = pageData[1];
+
+  console.log("O: ", objVal);
+
+  lineup.forEach((player) => {
+    let row = document.createElement("tr");
+    let statusCell = document.createElement("td");
+    let nameCell = document.createElement("td");
+    let projectedPointsCell = document.createElement("td");
+    let costCell = document.createElement("td");
+
+    objValDiv.textContent = "Model Objective Value: " + objVal.toString();
+
+    statusCell.textContent = `${player["Status"]}`;
+    nameCell.textContent = `${player["Name"]}`;
+    projectedPointsCell.textContent = `${player["Projected Points"]}`;
+    costCell.textContent = `${player["Cost"]}`;
+
+    row.appendChild(statusCell);
+    row.appendChild(nameCell);
+    row.appendChild(projectedPointsCell);
+    row.appendChild(costCell);
+
+    tableBody.appendChild(row);
+  });
+}
+
+function handleNextPage() {
+  if (currentPage < arrayData.length - 1) {
+    if (nextPageButton.disabled) {
+      nextPageButton.disabled = false;
+    }
+    currentPage++;
+    console.log("current page:", currentPage);
+    renderPage(currentPage);
+  } else {
+    nextPageButton.disabled = true;
+  }
+  // enable previous button if currentPage is greater than 0
+  if (currentPage > 0) {
+    prevPageButton.disabled = false;
+  }
+}
+
+function handlePrevPage() {
+  if (currentPage > 0) {
+    if (prevPageButton.disabled) {
+      prevPageButton.disabled = false;
+    }
+    currentPage--;
+    console.log("current page:", currentPage);
+    renderPage(currentPage);
+  } else {
+    prevPageButton.disabled = true;
+  }
+  if (currentPage < arrayData.length - 1) {
+    nextPageButton.disabled = false;
+  }
+}
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const file = fileInput.files[0];
+
   if (!file) {
     alert("Please select a file to upload.");
     return;
   }
+
   const formData = new FormData();
   formData.append("file", file);
   fetch("/", {
     method: "POST",
     body: formData,
+    responseType: "json",
   })
     .then((response) => response.json())
     .then((data) => {
-      data = data.data;
-      const tableBody = document.getElementById("table-body");
-      tableBody.innerHTML = "";
-      data.forEach((item) => {
-        const row = document.createElement("tr");
-        const statusCell = document.createElement("td");
-        const nameCell = document.createElement("td");
-        const projectedPointsCell = document.createElement("td");
-        const costCell = document.createElement("td");
-
-        statusCell.textContent = item.Status;
-        nameCell.textContent = item.Name;
-        projectedPointsCell.textContent = item["Projected Points"];
-        costCell.textContent = item.Cost;
-        row.appendChild(statusCell);
-        row.appendChild(nameCell);
-        row.appendChild(projectedPointsCell);
-        row.appendChild(costCell);
-
-        tableBody.appendChild(row);
-      });
+      arrayData = Object.entries(data);
+      arrayData.sort(([key1], [key2]) => key2 - key1);
+      objectData = Object.fromEntries(arrayData);
+      renderPage(currentPage);
     })
     .catch((error) => {
       console.error(error);
       alert("An error occurred while uploading the file.");
     });
 });
+
+nextPageButton.addEventListener("click", handleNextPage);
+prevPageButton.addEventListener("click", handlePrevPage);
